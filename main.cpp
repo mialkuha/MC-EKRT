a17b4c081cab7bf17eb5ffb05d96026010483406
Mikko Kuha <mikkokuha0@gmail.com>
2025-02-06 22:56:09 +0200

    Copyright and licensing update

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
-//Copyright (c) 2023 Mikko Kuha 
+// A Monte Carlo EKRT event generator, see https://doi.org/10.48550/arXiv.2406.17592
+// <Citation>. 
+// Copyright (c) 2025 Mikko Kuha.
+// This program is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software 
+// Foundation, either version 3 of the License, or (at your option) any later version.
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// See the GNU General Public License for more details. You should have received a copy
+// of the GNU General Public License along with this program. If not, see
+// <http://www.gnu.org/licenses/>.
+// 
+// This program uses OpenMP, which is licensed under the Apache License, Version 2.0 
+// (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 
 #include "mcaa.hpp"
 
 int main(int argc, char* argv[])
 {
+    std::cout<< "MC-EKRT: Copyright (C) 2025 Mikko Kuha. This program is published" <<std::endl;
+    std::cout<< "under GPLv3 license, and comes with ABSOLUTELY NO WARRANTY; This" <<std::endl;
+    std::cout<< "is free software, and you are welcome to redistribute it under" <<std::endl;
+    std::cout<< "certain conditions; For more information, see <http://www.gnu.org/licenses/>" <<std::endl;
+
     if (argc < 2)
     {
         mcaa sim("params_template");
--- src/mcaa.hpp
+++ src/mcaa.hpp
@@ -1,4 +1,26 @@
-// Copyright (c) 2023 Mikko Kuha
+// A Monte Carlo EKRT event generator, see https://doi.org/10.48550/arXiv.2406.17592
+// <Citation>. 
+// Copyright (c) 2025 Mikko Kuha.
+// This program is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software 
+// Foundation, either version 3 of the License, or (at your option) any later version.
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// See the GNU General Public License for more details. You should have received a copy
+// of the GNU General Public License along with this program. If not, see
+// <http://www.gnu.org/licenses/>.
+// 
+// This program uses OpenMP, which is licensed under the Apache License, Version 2.0 
+// (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef MCAA_HPP
 #define MCAA_HPP
